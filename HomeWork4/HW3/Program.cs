d905ec8836e7e5d61aad855869c405c28219ad3f
Alena <alena.susko@yandex.ru>
2022-12-14 01:15:54 +0300

    added third task

---
+++ HomeWork4/HW3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+var str = string.Join(", ", GetArray());
+Console.WriteLine("[" + str + "]");
