145c4532457bb0c6a29d0682e0bd5825afa9160e
Alena <alena.susko@yandex.ru>
2023-01-06 09:56:33 +0300

    added the first task

---
+++ HomeWork8/HW1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] Get2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortNumbers(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputMatrix.GetLength(1) - 1; k++)
+            {
+                if (inputMatrix[i, k] < inputMatrix[i, k + 1])
+                {
+                    int temp = inputMatrix[i, k + 1];
+                    inputMatrix[i, k + 1] = inputMatrix[i, k];
+                    inputMatrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] result = Get2DArray(rows, columns, 0, 10);
+Console.WriteLine("Исходный массив: ");
+Print2DArray(result);
+
+SortNumbers(result);
+Console.WriteLine("Измененный массив: ");
+Print2DArray(result);
+
+
+
